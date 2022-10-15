3108650dcbea214b29f9c84fcbe389eb144a26eb
Naman Arora <namanarora1022@gmail.com>
2022-10-15 19:06:54 +0530

    get current user done

--- server/src/users/controllers/users.controller.ts
+++ server/src/users/controllers/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthGuard } from '../../auth/auth.guard';
 import { UsersService } from '../services/users.service';
 import { excludeUserDetails } from '../utils';
 
@@ -9,5 +11,13 @@ export class UsersController {
     async users() {
         const users = await this.usersService.findAll();
         return users.map((user) => excludeUserDetails(user));
+    }
+
+    @Get('me')
+    @UseGuards(AuthGuard)
+    async me(@Req() req: Request) {
+        const { userId } = req.session;
+        const user = await this.usersService.findOneById(userId);
+        return excludeUserDetails(user);
     }
 }
