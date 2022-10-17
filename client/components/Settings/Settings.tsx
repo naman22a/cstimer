06630b37228c4546f62f349998488c6973d2bc44
Naman Arora <namanarora1022@gmail.com>
2022-10-17 20:26:30 +0530

    Settings styling done

--- client/components/Settings/Settings.tsx
+++ client/components/Settings/Settings.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from './Settings.module.scss';
+import { IoIosSettings } from 'react-icons/io';
+import { FiLogOut } from 'react-icons/fi';
+import { BsFillGrid3X3GapFill } from 'react-icons/bs';
+import { AiOutlineUnorderedList } from 'react-icons/ai';
+import { HiOutlineCurrencyRupee, HiOutlineAdjustments } from 'react-icons/hi';
 
 const Settings: React.FC = () => {
     return (
         <div className={styles.container}>
-            <h1>Settings</h1>
+            <button>
+                <IoIosSettings />
+            </button>
+            <button>
+                <FiLogOut />
+            </button>
+            <button>
+                <BsFillGrid3X3GapFill />
+            </button>
+
+            <button>CsTimer</button>
+
+            <button>
+                <AiOutlineUnorderedList />
+            </button>
+            <button>
+                <HiOutlineCurrencyRupee />
+            </button>
+            <button>
+                <HiOutlineAdjustments />
+            </button>
         </div>
     );
 };
