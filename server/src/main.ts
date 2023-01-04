5ef01abce77bf313a98527bd633bdd15f49d6458
Naman Arora <namanarora1022@gmail.com>
2023-01-04 17:36:58 +0530

    added cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 import * as session from 'express-session';
 import * as connectRedis from 'connect-redis';
+import * as cookieParser from 'cookie-parser';
 import { COOKIE_NAME, __prod__ } from './constants';
 import { redis } from './redis';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -49,18 +50,18 @@ async function bootstrap() {
         origin: process.env.WEBSITE_DOMAIN,
         credentials: true,
     });
+    app.use(cookieParser());
     app.use(
         session({
             name: COOKIE_NAME,
             secret: process.env.SESSION_SECRET,
             resave: false,
             cookie: {
-                sameSite: 'none',
+                sameSite: 'lax',
                 httpOnly: true,
                 secure: __prod__,
                 maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
                 domain: __prod__ ? process.env.COOKIE_DOMAIN : undefined,
-                path: '/login',
             },
             store: new RedisStore({ client: redis }),
             saveUninitialized: false,
